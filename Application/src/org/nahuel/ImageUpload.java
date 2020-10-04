1502ede36fddab77184abfcf71e088b7e9a680f0
Nahuel Ahumada <ahumada.nahuel35149@gmail.com>
2020-10-04 02:14:37 -0300

    Image Upload: Servlet

--- Application/src/org/nahuel/ImageUpload.java
+++ Application/src/org/nahuel/ImageUpload.java
@@ -1,21 +1,48 @@
 package org.nahuel;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
 
 @WebServlet("/ImageUpload")
 public class ImageUpload extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	public String path="D:/JavaCourseMaterial/Images/";
    
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
-		doGet(request, response);
+		ServletFileUpload upload= new ServletFileUpload(new DiskFileItemFactory());
+		
+		try {
+			List<FileItem> images=upload.parseRequest(request);
+			for(FileItem image: images) {
+				String name=image.getName();
+				try{name=name.substring(name.lastIndexOf("\\")+1);}catch(Exception e) {};
+				System.out.println(name);
+				try {
+					image.write(new File(path+name));
+				} catch (Exception e) {
+					System.out.println("Error archivo");
+					e.printStackTrace();
+				}
+			}
+		} catch (FileUploadException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	
 
 }
